ef817de72917c9600dedd90e82b323c9c2969ca6
Stas Rivkin <rsl14591@gmail.com>
2025-04-23 12:48:34 +0300

    init

---
+++ src/main/java/com/aman/silvenet_snpay/SilvenetSnpayApplication.java
@@ -0,0 +1,13 @@
+package com.aman.silvenet_snpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilvenetSnpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilvenetSnpayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aman/silvenet_snpay/SilvenetSnpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.silvenet_snpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilvenetSnpayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
